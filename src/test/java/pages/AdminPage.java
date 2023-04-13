52b7bbfe3266a1ba4813b10bab89cbce8f35c865
feyza-unal <feyzaunal95@gmail.com>
2023-04-13 21:29:11 +0300

    13/04

--- src/test/java/pages/AdminPage.java
+++ src/test/java/pages/AdminPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -7,4 +9,46 @@ public class AdminPage {
     public AdminPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "//button[text()='Menu']")
+    public WebElement menuButton;
+
+    @FindBy(xpath = "//a[text()='Admin Management']")
+    public WebElement adminManagement;
+
+    @FindBy(id = "name")
+    public WebElement nameBox;
+
+    @FindBy(id = "surname")
+    public WebElement surnameBox;
+
+    @FindBy(id = "birthPlace")
+    public WebElement birthPlaceBox;
+
+    @FindBy(xpath = "//label[text()='Female']")
+    public WebElement genderFemale;
+
+    @FindBy(xpath = "//label[text()='Male']")
+    public WebElement genderMale;
+
+    @FindBy(id = "birthDay")
+    public WebElement birthdayBox;
+
+    @FindBy(id = "phoneNumber")
+    public WebElement phoneNumberBox;
+
+    @FindBy(id = "ssn")
+    public WebElement ssnBox;
+
+    @FindBy(id = "username")
+    public WebElement usernameBox;
+
+    @FindBy(id = "password")
+    public WebElement passwordBox;
+
+    @FindBy(xpath = "//button[text()='Submit']")
+    public WebElement submitButton;
+
+
+
 }
