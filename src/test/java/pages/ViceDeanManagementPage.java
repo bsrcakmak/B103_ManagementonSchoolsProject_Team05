bcecec3e6d52a9709db0fd2ead369dd72e0338e6
Enesy8 <nsmaily8@gmail.com>
2023-04-15 02:09:55 +0300

    team05

--- src/test/java/pages/ViceDeanManagementPage.java
+++ src/test/java/pages/ViceDeanManagementPage.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
-public class ViceDeanPage {
+public class ViceDeanManagementPage {
 
-    public ViceDeanPage() {
+    public ViceDeanManagementPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
