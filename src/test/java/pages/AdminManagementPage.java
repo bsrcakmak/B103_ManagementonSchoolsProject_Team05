25a73a7e44d0812beeb5a0f70ace7b4d7890a296
Ahmet Can Ates <omerzed54@gmail.com>
2023-04-18 23:11:21 +0300

    v2

--- src/test/java/pages/AdminManagementPage.java
+++ src/test/java/pages/AdminManagementPage.java
@@ -27,7 +27,7 @@ public class AdminManagementPage {
     public WebElement genderMale;
 
     @FindBy(id = "birthDay")
-    public WebElement birthDayBox;
+    public WebElement dateOfBirthBox;
 
     @FindBy(id = "phoneNumber")
     public WebElement phoneNumber;
@@ -40,5 +40,11 @@ public class AdminManagementPage {
 
     @FindBy(id = "password")
     public WebElement passwordBox;
+
+    @FindBy(xpath = "(//*[text()='Required'])[1]")
+    public WebElement requiredFeedback;
+
+    @FindBy(xpath = "//*[@class='Toastify__toast-container Toastify__toast-container--top-center']")
+    public WebElement fail;
 
 }
